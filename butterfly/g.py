940f6ef74d8d674f75243ae93dfe1ce9377c3232
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-09-16 13:43:03 -0400

    Added gravity
    @TheodoreGalanos in our case since z vector is up gravity should be
    assigned as (0 0 -9.81) and not (0 -9.81 0) which was in the example
    file. Is that right?

---
+++ butterfly/g.py
@@ -0,0 +1,21 @@
+"""g[ravity] class."""
+from foamfile import ZeroFolderFoamFile
+from collections import OrderedDict
+
+
+class G(ZeroFolderFoamFile):
+    """G (gravity) class."""
+
+    # set default valus for this class
+    __defaultValues = OrderedDict()
+    __defaultValues['dimensions'] = '[0 1 -2 0 0 0 0]'
+    __defaultValues['#include'] = None
+    __defaultValues['value'] = '(0 0 -9.81)'
+
+    def __init__(self, values=None):
+        """Init class."""
+        ZeroFolderFoamFile.__init__(self, name='g',
+                                    cls='uniformDimensionedVectorField',
+                                    location='constant',
+                                    defaultValues=self.__defaultValues,
+                                    values=values)
