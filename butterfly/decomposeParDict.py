507839b461582c580eb5c41f3c3e4007cac75dc8
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-09-27 22:53:17 -0400

    Added decomposeParDict
    Currently scotch and simple modes are supported.

---
+++ butterfly/decomposeParDict.py
@@ -0,0 +1,57 @@
+"""decomposeParDict class.
+
+Decompose parameters for parallel runs.
+"""
+from foamfile import FoamFile
+from collections import OrderedDict
+
+
+class DecomposeParDict(FoamFile):
+    """DecomposeParDict class."""
+
+    # set default valus for this class
+    __defaultValues = OrderedDict()
+    __defaultValues['numberOfSubdomains'] = '2'
+    __defaultValues['method'] = 'scotch'
+
+    def __init__(self, values=None):
+        """Init class."""
+        FoamFile.__init__(self, name='decomposeParDict', cls='dictionary',
+                          location='system', defaultValues=self.__defaultValues,
+                          values=values)
+
+    @classmethod
+    def scotch(cls, numberOfSubdomains=2):
+        """Scotch method.
+
+        Args:
+            numberOfSubdomains: Total number of subdomains (default: 2).
+        """
+        values = {'method': 'scotch',
+                  'numberOfSubdomains': str(numberOfSubdomains)}
+        return cls(values=values)
+
+    @classmethod
+    def simple(cls, numberOfSubdomainsXYZ=None, delta=0.001):
+        """Simple method.
+
+        Args:
+            numberOfSubdomainsXYZ: Number of subdomains in x, y, z as a tuple
+                (default: (2, 1, 1))
+            delta: Cell skew factor (default: 0.001).
+        """
+        try:
+            numberOfSubdomainsXYZ = tuple(numberOfSubdomainsXYZ)
+        except:
+            numberOfSubdomainsXYZ = (2, 1, 1)
+
+        numberOfSubdomains = numberOfSubdomainsXYZ[0] * \
+            numberOfSubdomainsXYZ[1] * numberOfSubdomainsXYZ[2]
+
+        values = {'method': 'simple',
+                  'simpleCoeffs':
+                  {'numberOfSubdomains': str(numberOfSubdomains),
+                   'n': str(numberOfSubdomainsXYZ).replace(',', ' '),
+                   'delta': str(delta)}}
+
+        return cls(values=values)
