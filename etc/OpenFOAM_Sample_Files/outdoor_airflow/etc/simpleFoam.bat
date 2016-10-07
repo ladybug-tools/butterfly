a5bbf538b07b5e123d2b999dbacce1eb003d8c54
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-10-06 23:05:25 -0400

    Updated OpenFOAM cases to match the latest version
    Resolved #80!

---
+++ etc/OpenFOAM_Sample_Files/outdoor_airflow/etc/simpleFoam.bat
@@ -0,0 +1,12 @@
+@echo off
+cd C:\Program Files\Docker Toolbox
+echo Setting up the environment to connect to docker...
+echo .
+SET DOCKER_TLS_VERIFY=1
+SET DOCKER_HOST=tcp://192.168.99.100:2376
+SET DOCKER_CERT_PATH=C:\Users\Administrator\.docker\machine\machines\default
+SET DOCKER_MACHINE_NAME=default
+echo Done!
+echo Running OpenFOAM commands...
+echo .
+docker exec -i e7a36e8e9eeb su - ofuser -c "cd /home/ofuser/workingDir/butterfly/outdoor_airflow; simpleFoam  > >(tee etc/simpleFoam.log) 2> >(tee etc/simpleFoam.err >&2)"
