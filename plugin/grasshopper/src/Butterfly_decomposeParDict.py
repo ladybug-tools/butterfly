747a19160ad016c29d0b963ae7605774d3f9319a
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2017-01-08 16:09:17 -0500

    Fixed typeHints bugs in Grasshopper components

--- plugin/grasshopper/src/Butterfly_decomposeParDict.py
+++ plugin/grasshopper/src/Butterfly_decomposeParDict.py
@@ -19,7 +19,7 @@ decomposeParDict. Dictionary for parallel runs.
 
 ghenv.Component.Name = "Butterfly_decomposeParDict"
 ghenv.Component.NickName = "decomposeParDict"
-ghenv.Component.Message = 'VER 0.0.03\nOCT_30_2016'
+ghenv.Component.Message = 'VER 0.0.03\nJAN_08_2017'
 ghenv.Component.Category = "Butterfly"
 ghenv.Component.SubCategory = "07::Etc"
 ghenv.Component.AdditionalHelpFromDocStrings = "2"
