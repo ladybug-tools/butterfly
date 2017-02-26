5512f27950e5577678bae08fa6cf836854392be1
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2017-02-25 20:22:54 -0500

    Updated API documentation
    There is a bug in documentation script. couple of modules for
    grasshopper library didn't updated correctly.

--- update_doc.bat
+++ update_doc.bat
@@ -1,3 +1,4 @@
-c:\Python27\python.exe C:\Python27\Scripts\pdoc --all-submodules --html --html-dir ./doc --overwrite butterfly
+:: c:\Python27\python.exe C:\Python27\Scripts\pdoc --all-submodules --html --html-dir ./doc --overwrite butterfly
 c:\Python27\python.exe C:\Python27\Scripts\pdoc --all-submodules --html --html-dir ./doc --overwrite butterfly_grasshopper
+:: c:\Python27\python.exe C:\Python27\Scripts\pdoc --all-submodules --html --html-dir ./doc --overwrite butterfly_dynamo
 pause
