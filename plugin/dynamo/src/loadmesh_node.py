92ce096d3778cbeff3f6bf8b826ed1d367abc901
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2017-03-13 22:48:33 -0400

    Moved grasshopper and dynamo libs to new repo
    This change will break the Grasshopper installer!

--- plugin/dynamo/src/loadmesh_node.py
+++
@@ -1,9 +0,0 @@
-# assign inputs
-_case, innerMesh_, _load = IN
-mesh = None
-
-if _case and _load:
-    mesh = _case.loadMesh(innerMesh_)
-
-# assign outputs to OUT
-OUT = (mesh,)
--- plugin/dynamo/src/loadpoints_node.py
+++
@@ -1,9 +0,0 @@
-# assign inputs
-_case, _load = IN
-pts = None
-
-if _case and _load:
-    pts = _case.loadPoints()
-
-# assign outputs to OUT
-OUT = (pts,)
